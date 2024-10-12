1b8c7106f45378dbc9a224d6f7e1736ee975960a
shernjr <38554378+shernjr@users.noreply.github.com>
2024-10-12 17:40:29 +1100

    starting UI work. trying to add moving borders

--- Assets/Script/StartSceneScript/StartSceneMovement.cs
+++ Assets/Script/StartSceneScript/StartSceneMovement.cs
@@ -1,36 +1,37 @@
+using Script.PacMovement;
 using UnityEngine;
 
-namespace Script.PacMovement {
-    public class PacStudentMovement : MonoBehaviour {
+namespace Script.StartSceneScript {
+    public class StartSceneMovement : MonoBehaviour {
     
         public Transform pacStudent;
+        public Transform ghost;
         public float moveSpeed = 5f;
-        public AudioSource moveAudio;
+        public float ghostOffset = 1f; 
         public Animator pacAnimator; // Reference to the Animator
         private Vector3[] _positions;
         private int _currentTargetIndex;
 
         private Tweener _tweener;
         
-    
-        // Start is called before the first frame update
         void Start()
         {
-            _tweener = GetComponent<Tweener>();
+            _tweener = GetComponent<Tweener>(); 
             
-            /*StartPosition();
-            MoveToNextPos();*/
+            StartPosition();
+            MoveToNextPos();
         
         }
     
-        /*private void StartPosition() {
+        private void StartPosition() { // ISSUE : Ghost will move diagonally. need to add check to prevent movement before the other finishes.
             _positions = new Vector3[] {
-                new Vector3(-12.5f, 11.8f, 0), // top left
-                new Vector3(-12.5f, 8f, 0),    // bottom left
-                new Vector3(-7.5f, 8f, 0),     // bottom right
-                new Vector3(-7.5f, 11.7f, 0),  // top right
+                new Vector3(-7f, 5f, 0), // start
+                new Vector3(-7f, -5f, 0),
+                new Vector3(7f, -5f, 0), 
+                new Vector3(7f, 5f, 0), 
             };
-            pacStudent.position = _positions[0];
+            pacStudent.position = _positions[1];
+            ghost.position = _positions[0];
             _currentTargetIndex = 1;
         }
 
@@ -40,36 +41,32 @@ namespace Script.PacMovement {
             if (_tweener.AddTween(pacStudent, pacStudent.position, targetPos, moveSpeed)) {
                 Vector3 direction = (targetPos - pacStudent.position).normalized;
                 UpdateAnimator(direction);
-                PlayMovementAudio();
+            }
+            
+            
+            //Vector3 ghostTargetPos = pacStudent.position - new Vector3(ghostOffset, 0, 0);
+
+            if (_tweener.AddTween(ghost, ghost.position, targetPos, moveSpeed)) {
+                Vector3 direction = (targetPos - ghost.position).normalized;
+                UpdateAnimator(direction);
             }
 
             _currentTargetIndex = (_currentTargetIndex + 1) % _positions.Length;
-        }*/
-    
+        }
+        
         private void UpdateAnimator(Vector3 direction) {
             float moveX = direction.x;
             float moveY = direction.y;
             pacAnimator.SetFloat("moveX", moveX);
             pacAnimator.SetFloat("moveY", moveY);
         }
-
-        private void PlayMovementAudio() {
-            if (!moveAudio.isPlaying) {
-                moveAudio.loop = true;
-                moveAudio.Play();
-            }
-        }
-
-        public void StopMovementAudio() {
-            if (moveAudio.isPlaying) {
-                moveAudio.Stop();
-            }
-        }
-    
+        
         private void Update() {
-            /*if (!_tweener.TweenExists(pacStudent)) {
+            if (!_tweener.TweenExists(pacStudent)) {
                 MoveToNextPos();
-            }*/
+            }
         }
     }
 }
+
+
